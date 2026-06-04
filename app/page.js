7cd9192de4cd804ce4072873cd3c89288487f076
2403717610421018-source <2403717610421018@cit.edu.in>
2026-06-04 19:10:53 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,27 @@
+import About from "../components/About"
+
 export default function Home() {
   return (
-    <main style={{
-      height: "100vh",
-      background: "black",
-      color: "white",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column"
-    }}>
-      <h1 style={{ color: "cyan", fontSize: "50px" }}>
-        Nikola Tesla ⚡
-      </h1>
+    <>
+      <main style={{
+        height: "100vh",
+        background: "black",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}>
+        <h1 style={{ color: "cyan", fontSize: "50px" }}>
+          Nikola Tesla ⚡
+        </h1>
 
-      <p style={{ color: "gray" }}>
-        Genius of Electricity
-      </p>
-    </main>
+        <p style={{ color: "gray" }}>
+          Genius of Electricity
+        </p>
+      </main>
+
+      <About />
+    </>
   )
 }
-
