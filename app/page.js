9ef62fceaed088ed045de9bfa1df675592ab9d2c
2403717610421018-source <2403717610421018@cit.edu.in>
2026-06-04 19:08:59 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      background: "black",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <h1 style={{ color: "cyan", fontSize: "50px" }}>
+        Nikola Tesla ⚡
+      </h1>
+
+      <p style={{ color: "gray" }}>
+        Genius of Electricity
+      </p>
+    </main>
+  )
+}
+
